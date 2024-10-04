b58d8d0bb13f856a2c7557fd8d50f870b529e650
rossoromain <bif.rosso@gmail.com>
2024-10-04 17:19:08 +0200

    feat: signit-interface project init

---
+++ signit-interface/eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import parserTs from "@typescript-eslint/parser";
+
+// La configuration ESLint au format "flat config"
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: parserTs, // Utilisation du parser TypeScript pour les fichiers TS et TSX
+      globals: globals.browser, // Variables globales du navigateur (ex. window, document)
+    },
+    
+  },
+  // Appliquer les configurations recommandées de JavaScript
+  pluginJs.configs.recommended,
+
+  // Appliquer les configurations recommandées de TypeScript
+  // tseslint.configs.recommended,
+
+  // Appliquer les configurations recommandées de React (pour React et les Hooks)
+  // pluginReact.configs.flat.recommended,
+];
